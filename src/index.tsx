9f2531cab64f2f93e408f2f38af61d49b8175e8e
박효진 <gywlsp@MacBook-Pro.local>
2020-08-26 11:14:17 +0900

    feat: :sparkles: add SafeAreaView to HomeScreen

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import styled from 'styled-components/native';
 import {WebView} from 'react-native-webview';
 
 export default function HomeScreen() {
-  return <WebView source={{uri: 'https://pickk.one/'}} style={{flex: 1}} />;
+  return (
+    <Wrapper>
+      <WebView source={{uri: 'https://pickk.one/'}} style={{flex: 1}} />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.SafeAreaView({
+  flex: 1,
+});
